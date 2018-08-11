61b77cca77d70dbd318d615ed1b67c92cf27d0b3
fakepinge <fakepinge@gmail.com>
2018-08-11 20:49:24 +0800

    update .gitignore

--- Example/Pods/Target Support Files/FPVerticalView/FPVerticalView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FPVerticalView : NSObject
-@end
-@implementation PodsDummy_FPVerticalView
-@end
--- Example/Pods/Target Support Files/Pods-FPVerticalView_Tests/Pods-FPVerticalView_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FPVerticalView_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_FPVerticalView_Tests
-@end
